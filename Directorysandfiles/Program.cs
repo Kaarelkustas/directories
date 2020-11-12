5e73085f9791e616b3d3fa1e46e235bcf85f28c4
Opilane TTHK <opilane@tthk.ee>
2020-11-12 13:15:29 +0200

    f

---
+++ Directorysandfiles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace Directorysandfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootPath = @"C:\Users\opilane\samples";
+
+            string[] allDirectories = Directory.GetDirectories(rootPath);
+            ReadArray(allDirectories);
+
+
+            string[] includeNestadDirectories = Directory.GetDirectories(rootPath, "*", SearchOption.AllDirectories);
+            ReadArray(includeNestadDirectories);
+
+            string[] files = Directory.GetFiles(rootPath, "*.*", SearchOption.AllDirectories);
+            ReadArray(files);
+        }
+        public static void ReadArray(string[] array)
+        {
+            foreach(string line in array)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
